2fc50dfd2c529a7cf4b9fb8b15f62e544d6d9b82
Fabian Schierok <fabian.schierok@udo.edu>
2021-03-17 20:38:47 +0100

    added status effects

--- dnd_init_tracker/lib/db.dart
+++ dnd_init_tracker/lib/db.dart
@@ -1,4 +1,6 @@
-class Char {
+import 'package:flutter/material.dart';
+
+class Char2 {
   String name;
   double init;
 
@@ -8,22 +10,51 @@ class Char {
   }
 }
 
-class Char2 {
+class Char {
   String name;
-  int initiative;
-  bool blinded;
-  bool charmed;
-  bool deafend;
-  bool frightend;
-  bool grappled;
-  bool incapacitated;
-  bool invisible;
-  bool paralyzed;
-  bool petrified;
-  bool poisoned;
-  bool prone;
-  bool restrained;
-  bool stunned;
-  bool unconscious;
-  int exhaustion;
+  double init;
+  bool blinded = false;
+  bool charmed = false;
+  bool deafend = false;
+  bool frightend = false;
+  bool grappled = false;
+  bool incapacitated = false;
+  bool invisible = false;
+  bool paralyzed = false;
+  bool petrified = false;
+  bool poisoned = false;
+  bool prone = false;
+  bool restrained = false;
+  bool stunned = false;
+  bool unconscious = false;
+  int exhaustion = 0;
+  int maxHp;
+  int hp;
+
+  Char(String name, double init, int maxHp) {
+    this.name = name;
+    this.init = init;
+    this.maxHp = maxHp;
+    this.hp = maxHp;
+  }
+
+  void takeDmg(int dmg) {
+    if (dmg >= this.hp) {
+      this.hp = 0;
+      this.incapacitated = true;
+    } else {
+      this.hp = this.hp - dmg;
+    }
+  }
+
+  void getHealed(int heal) {
+    if (this.maxHp - this.hp <= heal) {
+      this.hp = this.maxHp;
+    } else {
+      this.hp = this.hp + heal;
+    }
+    if (incapacitated) {
+      incapacitated = false;
+    }
+  }
 }
--- dnd_init_tracker/lib/main.dart
+++ dnd_init_tracker/lib/main.dart
@@ -93,15 +93,220 @@ class _InitListState extends State<InitList> {
   }
 
   Widget _buildRow(db.Char char) {
-    return ListTile(
-      title: Text(char.name),
-      subtitle: Text(char.init.toString()),
-      trailing: Icon(Icons.remove_circle),
-      onLongPress: () => {
-        setState(() {
-          listHandler.removeChar(char);
-        })
-      },
+    const double iconHeight = 32;
+    const int iconShade = 850;
+    final dmgFieldController = TextEditingController();
+    int dmg = 0;
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+              title: Row(children: <Widget>[
+                Text(
+                  char.name,
+                  style: char.incapacitated
+                      ? TextStyle(
+                          fontStyle: FontStyle.italic, color: Colors.red[800])
+                      : null,
+                ),
+                Spacer(),
+                Expanded(
+                  child: TextField(
+                    controller: dmgFieldController,
+                    decoration: InputDecoration(hintText: "Δ TP"),
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+                IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () => setState(() {
+                          print(dmgFieldController.text);
+                          dmg = int.parse(dmgFieldController.text);
+                          char.getHealed(dmg);
+                          dmgFieldController.clear();
+                        })),
+                IconButton(
+                    icon: Icon(Icons.remove),
+                    onPressed: () => setState(() {
+                          dmg = int.parse(dmgFieldController.text);
+                          char.takeDmg(dmg);
+                          dmgFieldController.clear();
+                        }))
+              ]),
+              subtitle: Text("Init: " +
+                  char.init.toString() +
+                  " Trefferpunkte: " +
+                  char.hp.toString()),
+              trailing: IconButton(
+                onPressed: () => {
+                  setState(() {
+                    listHandler.removeChar(char);
+                  })
+                },
+                icon: Icon(Icons.delete_outline),
+              )),
+          Wrap(
+            children: [
+              IconButton(
+                icon: Image.asset(
+                  "assets/FinalBlinded.png",
+                  height: iconHeight,
+                  color: !char.blinded ? Colors.grey[iconShade] : null,
+                ),
+                onPressed: () => {
+                  setState(() {
+                    char.blinded = !char.blinded;
+                  })
+                },
+              ),
+              IconButton(
+                icon: Image.asset(
+                  "assets/Charmed.png",
+                  height: iconHeight,
+                  color: !char.charmed ? Colors.grey[iconShade] : null,
+                ),
+                onPressed: () => {
+                  setState(() {
+                    char.charmed = !char.charmed;
+                  })
+                },
+              ),
+              IconButton(
+                icon: Image.asset(
+                  "assets/FinalDeafened.png",
+                  height: iconHeight,
+                  color: !char.deafend ? Colors.grey[iconShade] : null,
+                ),
+                onPressed: () => {
+                  setState(() {
+                    char.deafend = !char.deafend;
+                  })
+                },
+              ),
+              IconButton(
+                icon: Image.asset(
+                  "assets/Frightened.png",
+                  height: iconHeight,
+                  color: !char.frightend ? Colors.grey[iconShade] : null,
+                ),
+                onPressed: () => {
+                  setState(() {
+                    char.frightend = !char.frightend;
+                  })
+                },
+              ),
+              IconButton(
+                icon: Image.asset(
+                  "assets/FinalGrappled.png",
+                  height: iconHeight,
+                  color: !char.grappled ? Colors.grey[iconShade] : null,
+                ),
+                onPressed: () => {
+                  setState(() {
+                    char.grappled = !char.grappled;
+                  })
+                },
+              ),
+              IconButton(
+                icon: Image.asset(
+                  "assets/FinalInvisible.png",
+                  height: iconHeight,
+                  color: !char.invisible ? Colors.grey[iconShade] : null,
+                ),
+                onPressed: () => {
+                  setState(() {
+                    char.invisible = !char.invisible;
+                  })
+                },
+              ),
+              IconButton(
+                icon: Image.asset(
+                  "assets/FinalParalyzed.png",
+                  height: iconHeight,
+                  color: !char.paralyzed ? Colors.grey[iconShade] : null,
+                ),
+                onPressed: () => {
+                  setState(() {
+                    char.paralyzed = !char.paralyzed;
+                  })
+                },
+              ),
+              IconButton(
+                icon: Image.asset(
+                  "assets/FinalPetrified.png",
+                  height: iconHeight,
+                  color: !char.petrified ? Colors.grey[iconShade] : null,
+                ),
+                onPressed: () => {
+                  setState(() {
+                    char.petrified = !char.petrified;
+                  })
+                },
+              ),
+              IconButton(
+                icon: Image.asset(
+                  "assets/FinalPoisoned.png",
+                  height: iconHeight,
+                  color: !char.poisoned ? Colors.grey[iconShade] : null,
+                ),
+                onPressed: () => {
+                  setState(() {
+                    char.poisoned = !char.poisoned;
+                  })
+                },
+              ),
+              IconButton(
+                icon: Image.asset(
+                  "assets/FinalProne.png",
+                  height: iconHeight,
+                  color: !char.prone ? Colors.grey[iconShade] : null,
+                ),
+                onPressed: () => {
+                  setState(() {
+                    char.prone = !char.prone;
+                  })
+                },
+              ),
+              IconButton(
+                icon: Image.asset(
+                  "assets/FinalRestrained.png",
+                  height: iconHeight,
+                  color: !char.restrained ? Colors.grey[iconShade] : null,
+                ),
+                onPressed: () => {
+                  setState(() {
+                    char.restrained = !char.restrained;
+                  })
+                },
+              ),
+              IconButton(
+                icon: Image.asset(
+                  "assets/FinalStunned.png",
+                  height: iconHeight,
+                  color: !char.stunned ? Colors.grey[iconShade] : null,
+                ),
+                onPressed: () => {
+                  setState(() {
+                    char.stunned = !char.stunned;
+                  })
+                },
+              ),
+              IconButton(
+                icon: Image.asset(
+                  "assets/Unconcious.png",
+                  height: iconHeight,
+                  color: !char.unconscious ? Colors.grey[iconShade] : null,
+                ),
+                onPressed: () => {
+                  setState(() {
+                    char.unconscious = !char.unconscious;
+                  })
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 
@@ -114,37 +319,40 @@ class _InitListState extends State<InitList> {
           ),
           body: Container(
             padding: EdgeInsets.all(64),
-            child: MyCustomForm(listHandler),
+            child: CharCreatorForm(listHandler),
           ));
     }));
     setState(() {});
   }
 }
 
-class MyCustomForm extends StatefulWidget {
-  MyCustomFormState myCustomFormState;
+class CharCreatorForm extends StatefulWidget {
+  CharCreatorFormState charCreatorFormState;
 
-  MyCustomForm(CharListHandler handler) {
-    myCustomFormState = MyCustomFormState(handler);
+  CharCreatorForm(CharListHandler handler) {
+    charCreatorFormState = CharCreatorFormState(handler);
   }
   @override
-  MyCustomFormState createState() => myCustomFormState;
+  CharCreatorFormState createState() => charCreatorFormState;
 }
 
-class MyCustomFormState extends State<MyCustomForm> {
+class CharCreatorFormState extends State<CharCreatorForm> {
   CharListHandler handler;
   final nameFieldContoller = TextEditingController();
   final initFieldContoller = TextEditingController();
+  final maxHpFieldController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
   var name = "";
   var init = 0.0;
+  int maxHp = 0;
 
-  MyCustomFormState(CharListHandler myHandler) {
+  CharCreatorFormState(CharListHandler myHandler) {
     handler = myHandler;
   }
   void clearText() {
     nameFieldContoller.clear();
     initFieldContoller.clear();
+    maxHpFieldController.clear();
   }
 
   @override
@@ -168,6 +376,14 @@ class MyCustomFormState extends State<MyCustomForm> {
             decoration: InputDecoration(hintText: "Initiative"),
             keyboardType: TextInputType.number,
           ),
+          TextFormField(
+            onSaved: (String value) {
+              maxHp = int.parse(value);
+            },
+            controller: maxHpFieldController,
+            decoration: InputDecoration(hintText: "Lebenspunkte"),
+            keyboardType: TextInputType.number,
+          ),
           Padding(
             padding: const EdgeInsets.all(16),
             child: ElevatedButton(
@@ -177,7 +393,7 @@ class MyCustomFormState extends State<MyCustomForm> {
                 ScaffoldMessenger.of(context).showSnackBar(
                     SnackBar(content: Text(name + " hinzugefügt")));
                 this.clearText();
-                handler.addChar(name, init);
+                handler.addChar(name, init, maxHp);
               },
               child: Text("Hinzufügen"),
             ),
@@ -194,8 +410,8 @@ class CharListHandler {
     this._charList = [];
   }
 
-  void addChar(String name, double init) {
-    _charList.add(db.Char(name, init));
+  void addChar(String name, double init, int maxHp) {
+    _charList.add(db.Char(name, init, maxHp));
     _sortList();
   }
 
